fe0d45f8168d3e7248d93cff4870af1a67f9f041
Glitch&Go <ritter.oxley1@gmail.com>
2025-06-22 13:54:49 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { getUserProfile, updateUserProfile } = require('../controllers/userController');
 
+router.get('/:id', getUserProfile);
+router.patch('/:id', updateUserProfile);
+
+module.exports = router;
