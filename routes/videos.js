94b938833e79a24a88b349200697211d2aba5926
Glitch&Go <ritter.oxley1@gmail.com>
2025-06-22 13:55:03 -0500

    Update videos.js

--- routes/videos.js
+++ routes/videos.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { getAllVideos, uploadVideo } = require('../controllers/videoController');
 
+router.get('/', getAllVideos);
+router.post('/', uploadVideo);
+
+module.exports = router;
